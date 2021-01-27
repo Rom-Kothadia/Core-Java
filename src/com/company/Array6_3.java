13d42c3677afd1df3beac63064db8d054b1b1a72
Rom-Kothadia <kothadiarom@gmail.com>
2021-01-27 17:15:22 -0500

    Chapter 6.3 completed

---
+++ src/com/company/Array6_3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Array6_3 {
+    public static void main(String[] args) {
+        int a[] = new int[3];
+        a[0]=2;
+        a[1]=3;
+        a[2]=5;
+        for (int i=0;i<3;i++)
+        System.out.println(a[i]);
+    }
+}
