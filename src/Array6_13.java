52803031e20499066ea6189eb4325a8d7349669f
Rom-Kothadia <kothadiarom@gmail.com>
2021-01-28 00:13:50 -0500

    Chapter 6.13 enhanced 3D array completed

---
+++ src/Array6_13.java
@@ -0,0 +1,24 @@
+public class Array6_13 {
+    public static void main(String[] args) {
+
+        int a[] = new int[5];
+        int b[][] = new int[2][2];
+        int c[][][] = new int[4][4][4];
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                for(int k=0;k<4;k++){
+                    c[i][j][k] = i+j+k;
+                }
+            }
+        }
+        for(int i[][] : c){
+            for(int j[] : i){
+                for(int k : j){
+                    System.out.print(" "+k);
+                }
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
