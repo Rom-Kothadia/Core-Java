d836f5608cde643bf566c7ac9a44c264b7145165
Rom-Kothadia <kothadiarom@gmail.com>
2021-01-28 00:41:31 -0500

    Chapter 6.15 types of main method  completed

---
+++ src/Array6_15.java
@@ -0,0 +1,8 @@
+public class Array6_15 {
+    public static void main(String[] args) {
+        //public static main(string ... args)
+        //static public main(string ... args)
+        //static public main(string[] args)
+        System.out.println("Hello Rom");
+    }
+}
