ed666ff208be1e542d1dd069852bb29d92456b63
Rom-Kothadia <kothadiarom@gmail.com>
2021-01-28 19:05:17 -0500

    Chapter 7.5 Default constructor completed

---
+++ src/Constructor7_5.java
@@ -0,0 +1,11 @@
+public class Constructor7_5 {
+    public static void main(String[] args) {
+        Rom obj = new Rom();//constructor
+    }
+}
+class Rom{
+    public Rom(){
+        //Default Constructor
+        System.out.println("Constructor called automatically at time of creating object");
+    }
+}
